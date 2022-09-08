5d67ff55394bc88bcb8625eb05ceacec5069ffe1
douglas <douglas.silva@projuris.com.br>
2022-09-08 20:28:52 -0300

    Knight

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -4,10 +4,7 @@ import chess.Color;
 import src.boardgame.Board;
 import src.boardgame.Piece;
 import src.boardgame.Position;
-import src.chess.pieces.Bishop;
-import src.chess.pieces.King;
-import src.chess.pieces.Pawn;
-import src.chess.pieces.Rook;
+import src.chess.pieces.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -77,7 +74,7 @@ public class ChessMatch {
 
         check = (testChek(opponent(currentPlayer))) ? true : false;
 
-        if (testChek(opponent(currentPlayer))) {
+        if (testChekMath(opponent(currentPlayer))) {
             checkMath = true;
         } else {
             nextTurn();
@@ -191,9 +188,11 @@ public class ChessMatch {
 
     private void initialSetup() {
         placeNewPiece('A', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('B', 1, new Knight(board, Color.WHITE));
         placeNewPiece('C', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('E', 1, new King(board, Color.WHITE));
         placeNewPiece('F', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('G', 1, new Knight(board, Color.WHITE));
         placeNewPiece('H', 1, new Rook(board, Color.WHITE));
         placeNewPiece('A', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('B', 2, new Pawn(board, Color.WHITE));
@@ -205,9 +204,11 @@ public class ChessMatch {
         placeNewPiece('H', 2, new Pawn(board, Color.WHITE));
 
         placeNewPiece('A', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('B', 8, new Knight(board, Color.BLACK));
         placeNewPiece('C', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('E', 8, new King(board, Color.BLACK));
         placeNewPiece('F', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('G', 8, new Knight(board, Color.BLACK));
         placeNewPiece('H', 8, new Rook(board, Color.BLACK));
         placeNewPiece('A', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('B', 7, new Pawn(board, Color.BLACK));
--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -12,6 +12,11 @@ public class Pawn extends ChessPiece {
     }
 
     @Override
+    public String toString() {
+        return "P";
+    }
+
+    @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
         Position p = new Position(0, 0);
@@ -58,8 +63,4 @@ public class Pawn extends ChessPiece {
         return mat;
     }
 
-    @Override
-    public String toString() {
-        return "P";
-    }
 }
